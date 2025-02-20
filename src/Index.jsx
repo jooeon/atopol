80c2f023321adf4e6f7fd60df9ddc3e9b3b68bd7
Park <jpark174@ur.rochester.edu>
2025-02-20 16:23:55 -0500

    Initial commit

---
+++ src/Index.jsx
@@ -0,0 +1,13 @@
+import { motion } from "motion/react"
+
+const Index = () => {
+    return (
+        <>
+            <main>
+                <h1 className="text-8xl">title page</h1>
+            </main>
+        </>
+    );
+};
+
+export default Index;
---
+++ tailwind.config.js
@@ -0,0 +1,72 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'selector',
+    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+    theme: {
+        fontFamily: {
+            // 'outfit': ['Outfit', 'Arial', 'system-ui', 'sans-serif'],
+        },
+        extend: {
+            width: {
+                '50vw': '50vw',
+                '90vw': '90vw',
+            },
+            maxWidth: {
+                'half': '50%',
+                '90vw': '90vw',
+            },
+            height: {
+                '500px':'500px',
+                '90vh': '90vh',
+            },
+            minHeight: {
+                '30vh': '30vh',
+            },
+            maxHeight: {
+                '90vh': '90vh',
+            },
+            spacing: {
+                '40rem' : '40rem',
+                '50rem' : '50rem',
+                '60rem' : '60rem',
+            },
+            fontSize: {
+                '35px': '35px',
+                '5xs': ['0.25rem', '0.667rem'],
+                '4xs': ['0.375rem', '0.667rem'],
+                '3xs': ['0.5rem', '0.75rem'],
+                '2xs': ['0.625rem', '0.75rem'],
+                '10xl': '10rem', /* 160px */
+                '11xl': '12rem', /* 192px */
+                '12xl': '13.5rem', /* 216px */
+                '13xl': '15rem', /* 240px */
+                '14xl': '17rem', /* 272px */
+                '15xl': '19rem', /* 304px */
+                '16xl': '21rem', /* 336px */
+                '17xl': '23rem', /* 368px */
+                '18xl': '25rem', /* 400px */
+            },
+            backdropBlur: {
+                xs: '0.75px',
+            },
+            colors: {
+                customWhite: "#fafafa",
+                customBlack: "#070707",
+                customBlackLight: "#151515",
+                customGray: "#9a9a9a",
+                customGrayLight: "#ececec",
+                customNavy: "#100f14",
+                customNavyLight: "#15141a",
+            },
+            screens: {
+                'xs': '390px',
+                '3xl': '2560px',	// 1440p (QHD)
+                '4xl': '3024px',	// MacBook Pro 14"
+                '5xl': '3456px',	// MacBook Pro 16"
+                '6xl': '3840px',	// 4k (UHD)
+                '7xl': '5120px',	// Studio Display
+            },
+        }
+    },
+    plugins: [], // Add any plugins here as needed
+};

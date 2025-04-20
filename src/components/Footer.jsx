0ec4000cf9113ced8f87d045bd9952ed33e02dab
Joo Eon <jooeon427@gmail.com>
2025-04-20 04:22:39 -0400

    - footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import { motion } from "framer-motion"
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className="z-30">
       <motion.div
-        className="flex items-center justify-between w-full p-3 md:p-5 text-3xs sm:text-xs md:text-sm xl:text-base"
+        className="flex items-center justify-between w-full p-3 md:p-5 text-4xs sm:text-3xs md:text-sm 3xl:text-lg 5xl:text-2xl 6xl:text-3xl"
         initial={{opacity: 0}}
         animate={{opacity: 1}}
         transition={{
@@ -14,6 +15,7 @@ const Footer = () => {
         }}
       >
         <p>Allen Topolski <span className="text-customOrange">&copy;{new Date().getFullYear()}</span></p>
+        <p>Design & Development: <Link to="https://jooeonpark.com" target="_blank" className="text-customOrange">Joo Eon Park</Link></p>
       </motion.div>
     </footer>
   );
